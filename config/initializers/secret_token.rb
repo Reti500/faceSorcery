21f9fac3cb69ff99ef4890eed3101ca3cb1a3bd8
root <root@fedora20.gateway.2wire.net>
2014-01-07 23:27:24 -0600

    Primera version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FaceSorcery::Application.config.secret_key_base = '7f0342a7117906b51875ebf3af2d2d0eaddf0f4c60f95036566fac134eb19fb040ff3cb4f041e10b179f2cdecfa51cb236455a4cfcb29396c2e7bb794e6cd0f8'
